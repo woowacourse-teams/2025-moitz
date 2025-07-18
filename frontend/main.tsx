7d46f9556f0c725362d96429acc9a821c5830740
Oliy <eunsoa0310@gmail.com>
2025-07-18 11:44:29 +0900

    chore: eslint 설정 업데이트
    - eslint.config.mjs 파일에서 TypeScript 및 React 관련 플러그인 추가
    - import/order 규칙 설정 추가
    - import/order 규칙은 `외부라이브러리`, `app`, `pages`, `widgets`, `features`, `shared/ (type/제외)`, `shared/types`, `css` 순서로 정렬됨.
    - 각 import group별로 개행 추가
    
    Co-authored-by: kaori-killer <kaori-killer@users.noreply.github.com>

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,9 +1,10 @@
-import App from './src/app/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from './src/app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
