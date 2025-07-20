da2d2f36541dad4f2ba1df354ccbd369a560b8cf
Oliy <eunsoa0310@gmail.com>
2025-07-20 15:50:29 +0900

    feat: Textarea 공통컴포넌트 개발
    Co-authored-by: kaori-killer <kaori-killer@users.noreply.github.com>

---
+++ frontend/src/shared/components/textarea/textarea.stories.ts
@@ -0,0 +1,25 @@
+import Textarea from './Textarea';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'shared/Textarea',
+  component: Textarea,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    placeholder: {
+      control: { type: 'text' },
+      description: '인풋 필드에 표시될 텍스트',
+    },
+  },
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { placeholder: '아무 값도 없어요' },
+};
