38233ef39a1e2441415df8632f8cc6eb572bc1eb
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-21 14:35:12 +0900

    style: reset.css를 import 함
    - reset.css를 GlobalStyle.tsx에서 Import 함

--- frontend/src/shared/styles/GlobalStyle.tsx
+++ frontend/src/shared/styles/GlobalStyle.tsx
@@ -1,9 +1,13 @@
 import { Global, css } from '@emotion/react';
+import './reset.css';
 
 function GlobalStyle() {
   return (
     <>
-      <link href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" rel="stylesheet" />
+      <link
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        rel="stylesheet"
+      />
       <Global
         styles={css`
           * {
