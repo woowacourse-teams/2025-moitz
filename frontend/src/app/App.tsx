ce2c1cfd15fc2b47dbca951cb4edc13ad5279e87
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-21 14:29:33 +0900

    chore: svg에 대한 타입 설정

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,10 @@
+import Tag from '../shared/components/tag/Tag';
 import GlobalStyle from '../shared/styles/GlobalStyle';
 
 export default function App() {
   return (
     <>
+      <Tag text="Hello" />
       <GlobalStyle />
     </>
   );
