124af761ec185b8c5d8c4e7ed9c94dca5fd96932
Oliy <eunsoa0310@gmail.com>
2025-07-23 14:43:44 +0900

    feat: main에서 Layout 컴포넌트 적용 및 GlobalStyle 적용
    - App 컴포넌트에서 Tag 및 GlobalStyle 제거 후 빈 div로 변경
    - 새로운 Layout 컴포넌트를 추가하여 App을 감싸고 GlobalStyle을 적용
    - 레이아웃 스타일을 정의하는 layout.styled.ts 파일 추가
    
    Co-authored-by: kaori-killer <kaori-killer@users.noreply.github.com>

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './src/app/App';
+import Layout from './src/shared/components/layout/Layout';
+import GlobalStyle from './src/shared/styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
 );
--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,3 @@
-import Tag from '../shared/components/tag/Tag';
-import GlobalStyle from '../shared/styles/GlobalStyle';
-
 export default function App() {
-  return (
-    <>
-      <Tag text="Hello" />
-      <GlobalStyle />
-    </>
-  );
+  return <div></div>;
 }
