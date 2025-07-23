8f67b6b25c62ee64c6fbb8d9c2884330a9b54455
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-23 17:12:15 +0900

    style: theme 타입 설정에서 발생하는 lint 에러를 끔

--- frontend/src/shared/styles/styled.d.ts
+++ frontend/src/shared/styles/styled.d.ts
@@ -4,5 +4,6 @@ import defaultTheme from './defaultTheme';
 type ThemeType = typeof defaultTheme;
 
 declare module '@emotion/react' {
-  export type Theme = ThemeType;
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface Theme extends ThemeType {}
 }
