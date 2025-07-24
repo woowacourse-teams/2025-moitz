9035e3076be7edf1dfad44ed6f4b91cc3932eb5d
Oliy <eunsoa0310@gmail.com>
2025-07-24 11:13:13 +0900

    feat: Logo 컴포넌트 개발

---
+++ frontend/src/shared/components/logo/Logo.tsx
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
+import { flex } from '@shared/styles/default.styled';
+
+import IconLogo from '@icons/logo-icon.svg';
+import IconLogoText from '@icons/logo-text.svg';
+
+function Logo() {
+  return (
+    <div css={[flex({ justify: 'center', align: 'center', gap: 5 })]}>
+      <img src={IconLogo} alt="logo-icon" />
+      <img src={IconLogoText} alt="logo-text" />
+    </div>
+  );
+}
+
+export default Logo;
