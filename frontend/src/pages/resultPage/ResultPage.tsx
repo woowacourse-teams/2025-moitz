46efa72a341fc26244bdbd1cac3649c86bebbdfb
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-24 15:02:17 +0900

    style: ResultPage 컴포넌트에 BottomSheet 위치 고정

--- frontend/src/pages/resultPage/ResultPage.tsx
+++ frontend/src/pages/resultPage/ResultPage.tsx
@@ -1,9 +1,22 @@
-import SpotItemList from '@features/spotItemList/SpotItemList';
+import BottomSheet from '@features/bottomSheet/BottomSheet';
+
+import { flex } from '@shared/styles/default.styled';
 
 import spotItemListMock from '../../mocks/spotItemListMock';
 
+import * as resultPage from './resultPage.styled';
+
 function ResultPage() {
-  return <SpotItemList itemList={spotItemListMock} />;
+  return (
+    <div
+      css={[
+        flex({ direction: 'column', justify: 'flex-end' }),
+        resultPage.base(),
+      ]}
+    >
+      <BottomSheet itemList={spotItemListMock} />
+    </div>
+  );
 }
 
 export default ResultPage;
---
+++ frontend/src/pages/resultPage/resultPage.styled.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const base = () => css`
+  width: 100%;
+  height: 100%;
+  background-color: red;
+`;
