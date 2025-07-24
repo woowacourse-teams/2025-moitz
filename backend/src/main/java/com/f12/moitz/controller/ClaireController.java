47e9c55dc98aaa68c4148c381860c11418f8e6ed
julie <7jujubuilds@gmail.com>
2025-07-24 10:53:26 +0900

    thur6

---
+++ backend/src/main/java/com/f12/moitz/controller/ClaireController.java
@@ -0,0 +1,18 @@
+package com.f12.moitz.controller;
+
+import com.f12.moitz.domain.Gumball;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/claire")
+public class ClaireController {
+
+    @GetMapping
+    public ResponseEntity<Gumball> get() {
+        return ResponseEntity.ok(new Gumball("Claire", 19));
+    }
+
+}
