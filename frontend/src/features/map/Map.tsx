111e5371b85c8fe0b0d7c3de386fa974530b3631
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-07-25 13:18:07 +0900

    feat: 지도 위에 레이아웃 추가

--- frontend/src/features/map/Map.tsx
+++ frontend/src/features/map/Map.tsx
@@ -1,26 +1,39 @@
 /** @jsxImportSource @emotion/react */
 
 import { useEffect, useRef } from 'react';
+import { Link } from 'react-router';
+
+import MapButton from '@shared/components/mapButton/MapButton';
+import MapPoint from '@shared/components/mapPoint/MapPoint';
+import { flex } from '@shared/styles/default.styled';
 
 import { spotItem } from '@shared/types/spotItem';
 
+import IconBack from '@icons/icon-back.svg';
+import IconShare from '@icons/icon-share.svg';
+
 import * as map from './map.styled';
 
-function Map({ itemList }: { itemList?: spotItem[] }) {
+interface MapProps {
+  itemList?: spotItem[];
+  showControls?: boolean;
+}
+
+function Map({ itemList = [], showControls = true }: MapProps) {
   const mapRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const { naver } = window;
-
     if (!naver || !mapRef.current) return;
 
-    const locations =
-      itemList?.map((item) => new naver.maps.LatLng(item.y, item.x)) ?? [];
+    const locations = itemList.map(
+      (item) => new naver.maps.LatLng(item.y, item.x),
+    );
     if (locations.length === 0) return;
 
     const map = new naver.maps.Map(mapRef.current, {
       center: locations[0],
-      zoom: 14,
+      zoom: 13,
     });
 
     locations.forEach((location) => {
@@ -29,9 +42,29 @@ function Map({ itemList }: { itemList?: spotItem[] }) {
         position: location,
       });
     });
-  }, []);
+  }, [itemList]);
 
-  return <div ref={mapRef} css={map.base()} />;
+  return (
+    <div css={map.container()}>
+      <div ref={mapRef} css={map.base()} />
+
+      {showControls && (
+        <>
+          <div css={[flex({ justify: 'space-between' }), map.top_overlay()]}>
+            <Link to="/">
+              <MapButton src={IconBack} alt="back" />
+            </Link>
+            <Link to="/">
+              <MapButton src={IconShare} alt="share" />
+            </Link>
+          </div>
+          <div css={[flex({ justify: 'space-between' }), map.bottom_overlay()]}>
+            <MapPoint text="전체 추첨 지점" />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Map;
--- frontend/src/features/map/map.styled.ts
+++ frontend/src/features/map/map.styled.ts
@@ -1,7 +1,29 @@
 import { css } from '@emotion/react';
 
+export const container = () => css`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
 export const base = () => css`
   position: absolute;
   width: 100%;
   height: 100%;
+`;
+
+export const top_overlay = () => css`
+  position: absolute;
+  width: 100%;
+  top: 20px;
+  padding: 0px 20px;
+  z-index: 10;
+`;
+
+export const bottom_overlay = () => css`
+  position: absolute;
+  width: 100%;
+  bottom: 15px;
+  padding: 0px 20px;
+  z-index: 10;
 `;
