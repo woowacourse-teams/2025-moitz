900f6e67063a9ff0f7f17a7a79ad210edf35570c
Oliy <eunsoa0310@gmail.com>
2025-07-29 11:20:48 +0900

    feat: Logo 컴포넌트 개발
    - Logo 컴포넌트를 새로 생성하고, 아이콘과 텍스트 로고를 포함
    - Logo 컴포넌트에 대한 스토리북 설정 추가

---
+++ frontend/src/shared/components/logo/logo.stories.ts
@@ -0,0 +1,19 @@
+import Logo from './Logo';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'shared/Logo',
+  component: Logo,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
