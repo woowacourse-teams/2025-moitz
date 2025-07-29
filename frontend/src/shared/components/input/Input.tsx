47ef200ff107488f3afe69714567e0549ba2d1e5
Oliy <eunsoa0310@gmail.com>
2025-07-29 17:00:54 +0900

    feat: Input 컴포넌트에 value, onChange, onKeyDown, onClick 속성 추가
    - InputProps에 새로운 속성 추가
    - 스토리북에서 인풋 필드의 기본값 및 이벤트 핸들러 설정

--- frontend/src/shared/components/input/Input.tsx
+++ frontend/src/shared/components/input/Input.tsx
@@ -1,15 +1,34 @@
 /** @jsxImportSource @emotion/react */
+import React from 'react';
+
 import { typography } from '@shared/styles/default.styled';
 
 import * as input from './input.styled';
 
 interface InputProps {
   placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onClick: () => void;
 }
 
-function Input({ placeholder }: InputProps) {
+function Input({
+  placeholder,
+  value,
+  onChange,
+  onKeyDown,
+  onClick,
+}: InputProps) {
   return (
-    <input css={[input.base(), typography.b1]} placeholder={placeholder} />
+    <input
+      css={[input.base(), typography.b1]}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      onClick={onClick}
+    />
   );
 }
 
