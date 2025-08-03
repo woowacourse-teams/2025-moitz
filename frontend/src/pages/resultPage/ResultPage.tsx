b94c4fe072fcb842585cca68c406f3aa5b906c69
kaori-killer <sksbthwjd@daum.net>
2025-08-04 08:22:06 +0900

    feat: resultPage에 Location 데이터 불러오고 분기 처리

--- frontend/src/pages/resultPage/ResultPage.tsx
+++ frontend/src/pages/resultPage/ResultPage.tsx
@@ -3,12 +3,39 @@ import BottomSheet from '@features/recommendation/components/bottomSheet/BottomS
 
 import { flex } from '@shared/styles/default.styled';
 
-import recommendedLocationsMock from '../../mocks/recommendedLocationsMock';
 import startingLocationsMock from '../../mocks/startingLocationsMock';
+
+import useLocations from '../../entities/hooks/useLocations';
+import { RecommendedLocation } from '../../entities/types/Location';
 
 import * as resultPage from './resultPage.styled';
 
+const requestBody = {
+  startingPlaceNames: ['강변역', '동대문역', '서울대입구역'],
+  meetingTime: '14:00',
+  requirement: '노래방은 있었으면 좋겠어요!',
+};
+
 function ResultPage() {
+  const { data: locations, loading, error } = useLocations(requestBody);
+
+  if (loading) return <p>로딩중...</p>;
+  if (error) return <p>에러 발생!</p>;
+  if (!locations || locations.length === 0) return <p>추천 결과가 없습니다.</p>;
+
+  const recommendedLocations: RecommendedLocation[] = locations.map(
+    (location) => ({
+      id: location.id,
+      index: location.index,
+      x: location.x,
+      y: location.y,
+      name: location.name,
+      avgMinutes: location.avgMinutes,
+      isBest: location.isBest,
+      description: location.description,
+      reason: location.reason,
+    }),
+  );
   return (
     <div
       css={[
@@ -18,11 +45,11 @@ function ResultPage() {
     >
       <Map
         startingLocations={startingLocationsMock}
-        recommendedLocations={recommendedLocationsMock}
+        recommendedLocations={recommendedLocations}
       />
       <BottomSheet
         startingLocations={startingLocationsMock}
-        recommendedLocations={recommendedLocationsMock}
+        recommendedLocations={recommendedLocations}
       />
     </div>
   );
