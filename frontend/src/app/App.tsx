617fb9ac38860e8fbd8f802c403bc9aa615dd744
Oliy <eunsoa0310@gmail.com>
2025-08-05 17:46:12 +0900

    feat: Google Analytics 적용 및 GA 태그를 index.html에 삽입

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,21 @@
-import { Route, Routes } from 'react-router';
+import { pageView } from '@config/gtag';
+import { useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router';
 
 import IndexPage from '@pages/indexPage/IndexPage';
 import NotFoundPage from '@pages/notFoundPage/NotFoundPage';
 import ResultPage from '@pages/resultPage/ResultPage';
 
 export default function App() {
+  if (process.env.NODE_ENV === 'production') {
+    const location = useLocation();
+
+    useEffect(() => {
+      pageView(new URL(window.location.href));
+    }, [location]);
+
+  }
+
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
