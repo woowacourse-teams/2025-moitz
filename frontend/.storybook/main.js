f663fc3b87667156b49c7615d63658b0bd45e212
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-08-05 13:04:32 +0900

    refactor: 스토리북 main.ts -> main.js로 변경하면서 CommonJs 방식으로 변경

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,29 +1,21 @@
-import { createRequire } from 'module';
-import path, { dirname, join } from 'path';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('path');
 
-import type { StorybookConfig } from '@storybook/react-webpack5';
-
-// 현재 파일이 존재하는 디렉토리의 절대 경로
-const require = createRequire(import.meta.url);
-const __dirname = path.dirname(require.resolve('./main.ts'));
-
-// 전달받은 패키지 이름(또는 모듈 이름)의 루트 디렉토리 절대 경로
-function getAbsolutePath(value: string) {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
-
-const config: StorybookConfig = {
+/** @type {import('@storybook/react-webpack5').StorybookConfig} */
+module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)', '../src/**/*.mdx'],
+
   addons: [
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-webpack5-compiler-babel'),
   ],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
+
   webpackFinal: async (config) => {
-    // Babel 설정 추가
     config.module?.rules?.push({
       test: /\.(ts|tsx)$/,
       exclude: /node_modules/,
@@ -37,10 +29,7 @@ const config: StorybookConfig = {
             ],
             [
               '@babel/preset-typescript',
-              {
-                onlyRemoveTypeImports: true,
-                allowDeclareFields: true,
-              },
+              { onlyRemoveTypeImports: true, allowDeclareFields: true },
             ],
             '@babel/preset-env',
           ],
@@ -49,7 +38,6 @@ const config: StorybookConfig = {
       },
     });
 
-    // 확장자 처리 추가
     config.resolve?.extensions?.push('.ts', '.tsx');
 
     if (config.resolve) {
@@ -74,4 +62,11 @@ const config: StorybookConfig = {
   },
 };
 
-export default config;
+/**
+ * 주어진 패키지 이름의 package.json 절대 경로를 기반으로 루트 디렉토리 경로 반환
+ * @param {string} pkg 패키지 이름 (e.g. '@storybook/addon-docs')
+ * @returns {string} 절대 경로
+ */
+function getAbsolutePath(pkg) {
+  return path.dirname(require.resolve(path.join(pkg, 'package.json')));
+}
