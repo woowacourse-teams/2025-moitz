7808b526f23a78e0bd93ee4b4c199c630caa9b93
Oliy <eunsoa0310@gmail.com>
2025-08-05 22:17:12 +0900

    feat: PlaceCard 컴포넌트 및 스타일 추가
    - 추천 장소 정보를 표시하는 PlaceCard 컴포넌트 구현

---
+++ frontend/src/features/recommendation/components/placeCard/placeCard.stories.ts
@@ -0,0 +1,29 @@
+import recommendedLocationsMock from '@mocks/recommendedLocationsMock';
+
+import PlaceCard from './PlaceCard';
+
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+const meta = {
+  title: 'features/PlaceCard',
+  component: PlaceCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    place: {
+      control: { type: 'object' },
+      description: '장소 정보',
+    },
+  },
+} satisfies Meta<typeof PlaceCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    place: recommendedLocationsMock[0].places[0],
+  },
+};
