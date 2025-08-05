409af8f846d41b3456f018a91b72cc4fc278675c
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-08-05 19:50:19 +0900

    chore: Jest 설정 파일에 별칭 설정

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -15,6 +15,17 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss)$': 'identity-obj-proxy',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@widgets/(.*)$': '<rootDir>/src/widgets/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@entities/(.*)$': '<rootDir>/src/entities/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@shared/components/(.*)$': '<rootDir>/src/shared/components/$1',
+    '^@shared/styles/(.*)$': '<rootDir>/src/shared/styles/$1',
+    '^@shared/types/(.*)$': '<rootDir>/src/shared/types/$1',
+    '^@icons/(.*)$': '<rootDir>/assets/icon/$1',
+    '^@mocks/(.*)$': '<rootDir>/src/mocks/$1',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
