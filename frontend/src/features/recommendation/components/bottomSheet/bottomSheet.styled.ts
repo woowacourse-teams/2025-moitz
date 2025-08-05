0662c0f6771ef22a99b72962a09864f6a57fc6f8
Oliy <eunsoa0310@gmail.com>
2025-08-05 19:57:38 +0900

    style: 바텀시트 내부 padding 수정

--- frontend/src/features/recommendation/components/bottomSheet/bottomSheet.styled.ts
+++ frontend/src/features/recommendation/components/bottomSheet/bottomSheet.styled.ts
@@ -5,7 +5,7 @@ import { colorToken, borderRadiusToken } from '@shared/styles/tokens';
 export const base = () => css`
   width: 100%;
   height: 50%;
-  padding: 24px 20px 0px 24px;
+  padding: 20px 20px 0px 20px;
   background-color: ${colorToken.gray[8]};
   border-top-left-radius: ${borderRadiusToken.input};
   border-top-right-radius: ${borderRadiusToken.input};
