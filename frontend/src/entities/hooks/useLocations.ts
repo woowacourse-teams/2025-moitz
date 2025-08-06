a8b038f98648452b8667170675609442ac50b841
kaori-killer <sksbthwjd@daum.net>
2025-08-07 06:52:43 +0900

    refactor: useLocations 훅에 return 타입 설정

--- frontend/src/entities/hooks/useLocations.ts
+++ frontend/src/entities/hooks/useLocations.ts
@@ -3,20 +3,20 @@ import { useEffect, useState } from 'react';
 import fetchLocations from '@entities/apis/fetchLocations';
 import { Location } from '@entities/types/Location';
 
-interface LocationRequestBody {
+type LocationRequestBody = {
   startingPlaceNames: string[];
   meetingTime: string;
   requirement: string;
-}
+};
 
-const useLocations = (
-  requestBody: LocationRequestBody,
-): {
+type useLocationsReturn = {
   data: Location[];
   loading: boolean;
   error: boolean;
   errorMessage: string;
-} => {
+};
+
+const useLocations = (requestBody: LocationRequestBody): useLocationsReturn => {
   const [data, setData] = useState<Location[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
