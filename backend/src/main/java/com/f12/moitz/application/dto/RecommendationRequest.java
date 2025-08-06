c752bf2721c1993a190301a91889613aa3900db1
egaeng09 <egaeng09@gmail.com>
2025-08-06 17:34:47 +0900

    feat: 만남 시간 필드 제거

--- backend/src/main/java/com/f12/moitz/application/dto/RecommendationRequest.java
+++ backend/src/main/java/com/f12/moitz/application/dto/RecommendationRequest.java
@@ -3,15 +3,12 @@ package com.f12.moitz.application.dto;
 import com.f12.moitz.common.error.exception.BadRequestException;
 import com.f12.moitz.common.error.exception.GeneralErrorCode;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalTime;
 import java.util.List;
 
 @Schema(description = "지역 추천 요청")
 public record RecommendationRequest(
         @Schema(description = "출발지 이름 목록", example = "[\"강변역\", \"동대문역\", \"서울대입구역\"]", requiredMode = Schema.RequiredMode.REQUIRED)
         List<String> startingPlaceNames,
-        @Schema(description = "도착 예정 시간", example = "14:00")
-        LocalTime meetingTime,
         @Schema(description = "추가 요청사항", example = "노래방은 있었으면 좋겠어요!")
         String requirement
 ) {
