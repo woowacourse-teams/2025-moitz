037a3df053ee607a15bb8a300afca881f3efcb48
kaori-killer <sksbthwjd@daum.net>
2025-08-07 07:02:13 +0900

    refactor: requestBody Mock 데이터 파일 분리

---
+++ frontend/src/mocks/RequestBodyMock.ts
@@ -0,0 +1,5 @@
+export const RequestBodyMock = {
+  startingPlaceNames: ['강변역', '동대문역', '서울대입구역'],
+  meetingTime: '14:00',
+  requirement: '노래방은 있었으면 좋겠어요!',
+};
--- frontend/src/pages/resultPage/ResultPage.tsx
+++ frontend/src/pages/resultPage/ResultPage.tsx
@@ -4,21 +4,15 @@ import BottomSheet from '@features/recommendation/components/bottomSheet/BottomS
 import useLocations from '@entities/hooks/useLocations';
 import { RecommendedLocation } from '@entities/types/Location';
 
-
 import { flex } from '@shared/styles/default.styled';
 
+import { RequestBodyMock } from '@mocks/RequestBodyMock';
 import startingLocationsMock from '@mocks/startingLocationsMock';
 
 import * as resultPage from './resultPage.styled';
 
-const requestBody = {
-  startingPlaceNames: ['강변역', '동대문역', '서울대입구역'],
-  meetingTime: '14:00',
-  requirement: '노래방은 있었으면 좋겠어요!',
-};
-
 function ResultPage() {
-  const { data: locations, loading, error } = useLocations(requestBody);
+  const { data: locations, loading, error } = useLocations(RequestBodyMock);
 
   if (loading) return <p>로딩중...</p>;
   if (error) return <p>에러 발생!</p>;
