0faf7729abc1a11e7de5b5b6241b8213cf38ac6a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-08-07 14:35:55 +0900

    [FE] feat: [공통 컴포넌트] 마커 인덱스 컴포넌트를 피그마 수정에 맞게 props 추가

--- frontend/src/shared/components/markerIndex/MarkerIndex.tsx
+++ frontend/src/shared/components/markerIndex/MarkerIndex.tsx
@@ -1,22 +1,51 @@
-import { flex, typography } from '@shared/styles/default.styled';
+import { flex, typography, shadow } from '@shared/styles/default.styled';
 
 import * as marker from './markerIndex.styled';
+import { label_base } from './markerIndex.styled';
 
 interface MarkerIndexProps {
   index: number;
+  type: 'recommended' | 'starting';
+  label?: string;
   hasStroke?: boolean;
+  hasShadow?: boolean;
 }
 
-function MarkerIndex({ index, hasStroke = false }: MarkerIndexProps) {
+function MarkerIndex({
+  index,
+  type,
+  label = '',
+  hasStroke = false,
+  hasShadow = false,
+}: MarkerIndexProps) {
+  const circle_type =
+    type === 'recommended' ? marker.circle_recommended : marker.circle_starting;
+  const label_type =
+    type === 'recommended' ? marker.label_recommended : marker.label_starting;
+
   return (
     <div
       css={[
-        flex({ justify: 'center', align: 'center' }),
-        marker.base(),
-        hasStroke && marker.stroke(),
+        flex({
+          direction: 'column',
+          justify: 'center',
+          align: 'center',
+          gap: 5,
+        }),
       ]}
     >
-      <span css={typography.h3}>{index}</span>
+      <div
+        css={[
+          flex({ justify: 'center', align: 'center' }),
+          marker.circle_base(),
+          circle_type(),
+          hasStroke && marker.stroke(),
+          hasShadow && shadow.map,
+        ]}
+      >
+        <span css={typography.h3}>{index}</span>
+      </div>
+      {label && <p css={[label_base(), label_type()]}>{label}</p>}
     </div>
   );
 }
