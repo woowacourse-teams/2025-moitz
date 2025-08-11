60c06b5c55e4c52b245d6917b006d053925bbc33
Oliy <eunsoa0310@gmail.com>
2025-08-11 21:04:55 +0900

    refactor: ProgressText 컴포넌트 추가 및 ProgressLoading에서 텍스트 표시 방식 변경
    - ProgressText 컴포넌트 생성
    - ProgressLoading 컴포넌트에서 기존 텍스트 표시 방식을 ProgressText로 대체

--- frontend/src/features/loading/components/progressLoading/ProgressLoading.tsx
+++ frontend/src/features/loading/components/progressLoading/ProgressLoading.tsx
@@ -1,30 +1,17 @@
-import { useEffect, useState } from 'react';
-
 import BaseLoading from '@features/loading/components/baseLoading/BaseLoading';
 import { LOADING_TEXT } from '@features/loading/constant/loadingText';
 import Progressbar from '@features/progressbar/components/progressbar/Progressbar';
 import { useProgress } from '@features/progressbar/hooks/useProgress';
 
-import { typography } from '@shared/styles/default.styled';
-
-import * as loading from './progressLoading.styled';
+import ProgressText from '../progressText/ProgressText';
 
 function ProgressLoading() {
   const { progress } = useProgress();
-  const [textIndex, setTextIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTextIndex((prev) => (prev + 1) % LOADING_TEXT.length);
-    }, 600);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <BaseLoading>
       <Progressbar progress={progress} />
-      <div css={[typography.b2, loading.text()]}>{LOADING_TEXT[textIndex]}</div>
+      <ProgressText text={LOADING_TEXT} />
     </BaseLoading>
   );
 }
