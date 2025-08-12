723b95179028e4ae2616c5022a85250fca42584d
kaori-killer <sksbthwjd@daum.net>
2025-08-12 22:03:38 +0900

    refactor: 불필요한 데이터 처리 작업 제거

--- frontend/src/pages/resultPage/ResultPage.tsx
+++ frontend/src/pages/resultPage/ResultPage.tsx
@@ -2,7 +2,7 @@ import Map from '@features/map/components/map/Map';
 import BottomSheet from '@features/recommendation/components/bottomSheet/BottomSheet';
 
 import { useLocationsContext } from '@entities/contexts/useLocationsContext';
-import { RecommendedLocation, StartingPlace } from '@entities/types/Location';
+import { RecommendedLocation } from '@entities/types/Location';
 
 import { flex } from '@shared/styles/default.styled';
 
@@ -16,17 +16,7 @@ function ResultPage() {
   if (!location || location.recommendedLocations.length === 0)
     return <p>추천 결과가 없습니다.</p>;
 
-  const startingPlaces: StartingPlace[] = location.startingPlaces.map(
-    (location) => {
-      return {
-        id: location.id,
-        index: location.index,
-        x: location.x,
-        y: location.y,
-        name: location.name,
-      };
-    },
-  );
+  const { startingPlaces } = location;
 
   const recommendedLocations: RecommendedLocation[] =
     location.recommendedLocations.map((location) => {
