84822ccff50666860098d4d34589d95ff7b75731
Oliy <eunsoa0310@gmail.com>
2025-08-12 15:15:34 +0900

    refactor: 컨벤션에 맞게 css 속성 선언 순서 변경

--- frontend/src/features/recommendation/components/routeIndicator/routeIndicator.styled.ts
+++ frontend/src/features/recommendation/components/routeIndicator/routeIndicator.styled.ts
@@ -5,6 +5,6 @@ import { colorToken } from '@shared/styles/tokens';
 export const base = () => css`
   width: 2px;
   height: 10px;
-  background-color: ${colorToken.gray[7]};
   margin: 0 4px;
+  background-color: ${colorToken.gray[7]};
 `;
