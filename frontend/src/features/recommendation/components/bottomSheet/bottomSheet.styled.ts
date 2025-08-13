d2d51c44e445cbf525333648fe3f8968e6be1b97
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-08-13 20:14:33 +0900

    feat: bottomSheet 퍼블리싱

--- frontend/src/features/recommendation/components/bottomSheet/bottomSheet.styled.ts
+++ frontend/src/features/recommendation/components/bottomSheet/bottomSheet.styled.ts
@@ -6,10 +6,28 @@ export const container = () => css`
   width: 100%;
   height: 50vh;
   min-height: 50vh;
-  padding: 20px 20px 0px 20px;
+  padding: 0px 20px 0px 20px;
   background-color: ${colorToken.gray[8]};
   border-top-left-radius: ${borderRadiusToken.input};
   border-top-right-radius: ${borderRadiusToken.input};
+`;
+
+export const header = () => css`
+  padding: 10px 0px 20px 0px;
+  cursor: grab;
+
+  &:active {
+    cursor: grabbing;
+  }
+`;
+
+export const handle = () => css`
+  width: 40px;
+  height: 4px;
+  background-color: ${colorToken.gray[7]};
+  border-radius: 2px;
+  margin: 8px auto;
+  display: block;
 `;
 
 export const content = () => css`
