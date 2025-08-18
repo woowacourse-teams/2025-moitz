9600582efe2db36484dd88e5bec7567f813f35d0
saera <yook.saera@gmail.com>
2025-08-18 16:56:57 +0900

    feat: 지하철 노선도 구축 기능 구현

---
+++ backend/src/main/java/com/f12/moitz/subway/SubwayStation.java
@@ -0,0 +1,46 @@
+package com.f12.moitz.subway;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import lombok.Getter;
+
+@Getter
+public class SubwayStation {
+
+    private final String name;
+    private final List<Edge> possibleEdges;
+
+    public SubwayStation(String name) {
+        this.name = name;
+        this.possibleEdges = new ArrayList<>();
+    }
+
+    @JsonCreator
+    public SubwayStation(
+            @JsonProperty("name") String name,
+            @JsonProperty("possibleEdges") List<Edge> possibleEdges
+    ) {
+        this.name = name;
+        this.possibleEdges = possibleEdges != null ? possibleEdges : new ArrayList<>();
+    }
+
+    public void addEdge(Edge newEdge) {
+        Optional<Edge> existingEdge = possibleEdges.stream()
+                .filter(edge -> edge.isEqualTo(newEdge))
+                .findFirst();
+
+        if (existingEdge.isEmpty()) {
+            possibleEdges.add(newEdge);
+        }
+    }
+
+    public Optional<Edge> findEdgeTo(final String destination) {
+        return possibleEdges.stream()
+                .filter(edge -> edge.isTowards(destination))
+                .findFirst();
+    }
+
+}
---
+++ backend/src/test/java/com/f12/moitz/subway/SubwayMapBuilderTest.java
@@ -0,0 +1,37 @@
+package com.f12.moitz.subway;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.f12.moitz.infrastructure.client.open.OpenApiClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SubwayMapBuilderTest {
+
+    @Autowired
+    private OpenApiClient openApiClient;
+
+    @DisplayName("지하철 노선도 업데이트")
+    @Test
+    void updateSubwayMap() {
+        // Given
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules(); // JSR310 모듈 등록
+
+        SubwayMapBuilder subwayMapBuilder = new SubwayMapBuilder(openApiClient, objectMapper);
+
+        // When
+        Map<String, SubwayStation> subwayMap = subwayMapBuilder.build();
+
+        // Then
+        assertThat(subwayMap).isNotEmpty();
+    }
+
+}
