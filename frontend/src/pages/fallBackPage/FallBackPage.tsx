3ba4cd0f3dfc209852e507c51fe2e10183bdd4c7
Oliy <eunsoa0310@gmail.com>
2025-08-21 05:55:18 +0900

    style: FallBackPage 스타일 및 내용 개선

--- frontend/src/pages/fallBackPage/FallBackPage.tsx
+++ frontend/src/pages/fallBackPage/FallBackPage.tsx
@@ -1,5 +1,6 @@
 import BottomButton from '@shared/components/bottomButton/BottomButton';
 import Layout from '@shared/components/layout/Layout';
+import Logo from '@shared/components/logo/Logo';
 import { flex, typography } from '@shared/styles/default.styled';
 
 import * as fallBackPage from './fallBackPage.styled';
@@ -18,24 +19,40 @@ function FallBackPage({
         css={[
           flex({
             direction: 'column',
-            justify: 'center',
+            justify: 'space-between',
             align: 'center',
             gap: 40,
           }),
           fallBackPage.base(),
         ]}
       >
-        <p css={typography.h1}>앱에 문제가 발생했어요.</p>
+        <Logo type="black" />
         <div
-          css={flex({
-            direction: 'column',
-            justify: 'center',
-            align: 'center',
-          })}
+          css={[
+            flex({
+              direction: 'column',
+              justify: 'center',
+              align: 'center',
+              gap: 30,
+            }),
+            fallBackPage.content(),
+          ]}
         >
-          {error.message}
-          <BottomButton onClick={reset} type="button" text="새로고침" active />
+          <p css={typography.h1}>일시적인 오류가 발생했어요</p>
+          <span css={[typography.b1, fallBackPage.description()]}>
+            잠시 후 다시 시도해 주시겠어요?
+            <br />
+            계속 문제가 발생한다면 모잇지에 알려주세요!
+            <br />
+            {error?.message}
+          </span>
         </div>
+        <BottomButton
+          type="button"
+          text="다시 시도하기"
+          active
+          onClick={reset}
+        />
       </div>
     </Layout>
   );
