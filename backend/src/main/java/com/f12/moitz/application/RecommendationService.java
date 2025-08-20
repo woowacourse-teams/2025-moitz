382c68ba63952edf6d1a208b867e3238a82c2f34
saera <yook.saera@gmail.com>
2025-08-20 22:14:44 +0900

    feat: PlaceService 생성 및 PlaceFinder를 대체

--- backend/src/main/java/com/f12/moitz/application/RecommendationService.java
+++ backend/src/main/java/com/f12/moitz/application/RecommendationService.java
@@ -4,7 +4,6 @@ import com.f12.moitz.application.dto.RecommendationRequest;
 import com.f12.moitz.application.dto.RecommendationsResponse;
 import com.f12.moitz.application.dto.RecommendedLocationsResponse;
 import com.f12.moitz.application.port.LocationRecommender;
-import com.f12.moitz.application.port.PlaceFinder;
 import com.f12.moitz.application.port.PlaceRecommender;
 import com.f12.moitz.application.port.RouteFinder;
 import com.f12.moitz.application.port.dto.ReasonAndDescription;
@@ -33,7 +32,8 @@ import org.springframework.util.StopWatch;
 @Slf4j
 @Service
 public class RecommendationService {
-    private final PlaceFinder placeFinder;
+
+    private final PlaceService placeService;
     private final PlaceRecommender placeRecommender;
     private final LocationRecommender locationRecommender;
     private final RouteFinder routeFinder;
@@ -42,14 +42,14 @@ public class RecommendationService {
     private final RecommendResultRepository recommendResultRepository;
 
     public RecommendationService(
-            @Autowired final PlaceFinder placeFinder,
+            @Autowired final PlaceService placeService,
             @Qualifier("placeRecommenderAdapter") final PlaceRecommender placeRecommender,
             @Autowired final LocationRecommender locationRecommender,
             @Qualifier("subwayRouteFinderAdapter") final RouteFinder routeFinder,
             @Autowired final RecommendationMapper recommendationMapper,
             @Autowired RecommendResultRepository recommendResultRepository
     ) {
-        this.placeFinder = placeFinder;
+        this.placeService = placeService;
         this.placeRecommender = placeRecommender;
         this.locationRecommender = locationRecommender;
         this.routeFinder = routeFinder;
@@ -62,15 +62,22 @@ public class RecommendationService {
         stopWatch.start("지역 추천");
         final String requirement = RecommendCondition.fromTitle(request.requirement()).getKeyword();
 
-        final List<Place> startingPlaces = placeFinder.findPlacesByNames(request.startingPlaceNames());
+        final List<Place> startingPlaces = placeService.findByNames(request.startingPlaceNames());
 
-        final RecommendedLocationsResponse recommendedLocationsResponse = locationRecommender.getRecommendedLocations(
+        final RecommendedLocationsResponse recommendedLocationsResponse = locationRecommender.recommendLocations(
                 request.startingPlaceNames(),
                 requirement
         );
 
-        final Map<Place, ReasonAndDescription> generatedPlacesWithReason = locationRecommender.recommendLocations(
-                recommendedLocationsResponse);
+        final Map<Place, ReasonAndDescription> generatedPlacesWithReason = recommendedLocationsResponse.recommendations()
+                .stream()
+                .collect(Collectors.toMap(
+                        recommendation -> placeService.findByName(recommendation.locationName()),
+                        recommendation -> new ReasonAndDescription(
+                                recommendation.reason(),
+                                recommendation.description()
+                        )
+                ));
         stopWatch.stop();
 
         final List<Place> generatedPlaces = generatedPlacesWithReason.keySet().stream().toList();
@@ -99,7 +106,7 @@ public class RecommendationService {
         stopWatch.stop();
         System.out.println(stopWatch.prettyPrint());
 
-        Result result = recommendationMapper.toResult(startingPlaces, recommendation, generatedPlacesWithReason);
+        final Result result = recommendationMapper.toResult(startingPlaces, recommendation, generatedPlacesWithReason);
         return recommendResultRepository.saveAndReturnId(result);
     }
 
@@ -159,8 +166,9 @@ public class RecommendationService {
     }
 
     public RecommendationsResponse findResultById(final String id) {
-        Result result = recommendResultRepository.findById(id)
+        final Result result = recommendResultRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("아이디에 해당하는 결과를 찾을 수 없습니다."));
         return new RecommendationsResponse(result.getStartingPlaces(), result.getRecommendedLocations());
     }
+
 }
--- backend/src/main/java/com/f12/moitz/infrastructure/adapter/LocationRecommenderAdapter.java
+++ backend/src/main/java/com/f12/moitz/infrastructure/adapter/LocationRecommenderAdapter.java
@@ -2,16 +2,10 @@ package com.f12.moitz.infrastructure.adapter;
 
 import com.f12.moitz.application.dto.RecommendedLocationsResponse;
 import com.f12.moitz.application.port.LocationRecommender;
-import com.f12.moitz.application.port.PlaceFinder;
-import com.f12.moitz.application.port.dto.ReasonAndDescription;
 import com.f12.moitz.common.error.exception.RetryableApiException;
-import com.f12.moitz.domain.Place;
 import com.f12.moitz.infrastructure.client.gemini.GoogleGeminiClient;
-import com.f12.moitz.infrastructure.client.gemini.dto.RecommendedLocationResponse;
 import com.f12.moitz.infrastructure.client.perplexity.PerplexityClient;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Recover;
@@ -26,15 +20,13 @@ public class LocationRecommenderAdapter implements LocationRecommender {
     private final GoogleGeminiClient geminiClient;
     private final PerplexityClient perplexityClient;
 
-    private final PlaceFinder placeFinder;
-
     @Retryable(
             retryFor = RetryableApiException.class,
             maxAttempts = 2,
             recover = "recoverRecommendedLocations"
     )
     @Override
-    public RecommendedLocationsResponse getRecommendedLocations(
+    public RecommendedLocationsResponse recommendLocations(
             final List<String> startPlaceNames,
             final String condition
     ) {
@@ -53,18 +45,6 @@ public class LocationRecommenderAdapter implements LocationRecommender {
                 startPlaceNames,
                 condition
         );
-    }
-
-    @Override
-    public Map<Place, ReasonAndDescription> recommendLocations(RecommendedLocationsResponse recommendedLocationsResponse) {
-        return recommendedLocationsResponse.recommendations().stream()
-                .collect(Collectors.toMap(recommendation ->
-                        placeFinder.findPlaceByName(recommendation.locationName()),
-                        recommendation -> new ReasonAndDescription(
-                                recommendation.reason(),
-                                recommendation.description()
-                        )
-                ));
     }
 
 }
