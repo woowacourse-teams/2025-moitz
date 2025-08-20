1f114e693b09c3bd4d4bfb24d52977f7ab488564
Oliy <eunsoa0310@gmail.com>
2025-08-21 05:45:18 +0900

    style: NotFoundPage 스타일 및 내용 수정

--- frontend/src/pages/components/notFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/components/notFoundPage/NotFoundPage.tsx
@@ -1,35 +1,50 @@
-import { Link } from 'react-router';
+import { useNavigate } from 'react-router';
 
+import BottomButton from '@shared/components/bottomButton/BottomButton';
+import Logo from '@shared/components/logo/Logo';
 import { flex, typography } from '@shared/styles/default.styled';
 
 import * as notFoundPage from './notFoundPage.styled';
 
 function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
     <div
       css={[
         flex({
           direction: 'column',
-          justify: 'center',
+          justify: 'space-between',
           align: 'center',
-          gap: 40,
         }),
         notFoundPage.base(),
       ]}
     >
-      <p css={typography.h1}>죄송합니다. 페이지를 사용할 수 없습니다.</p>
+      <Logo type="black" />
       <div
-        css={flex({
-          direction: 'column',
-          justify: 'center',
-          align: 'center',
-        })}
+        css={[
+          flex({
+            direction: 'column',
+            justify: 'center',
+            align: 'center',
+            gap: 30,
+          }),
+          notFoundPage.content(),
+        ]}
       >
-        <span css={typography.b1}>
-          클릭하신 링크가 잘못되었거나 페이지가 삭제되었습니다.
+        <p css={typography.h1}>앗! 찾으시는 페이지가 없어요</p>
+        <span css={[typography.b1, notFoundPage.description()]}>
+          주소가 변경되었거나 더는 제공되지 않는 페이지예요.
+          <br />
+          모잇지와 함께 새로운 약속 장소를 찾아볼까요?
         </span>
-        <Link to="/">모잇지로 돌아가기.</Link>
       </div>
+      <BottomButton
+        type="button"
+        text="홈으로 가기"
+        active
+        onClick={() => navigate('/')}
+      />
     </div>
   );
 }
--- frontend/src/pages/components/notFoundPage/notFoundPage.styled.ts
+++ frontend/src/pages/components/notFoundPage/notFoundPage.styled.ts
@@ -1,5 +1,17 @@
 import { css } from '@emotion/react';
 
 export const base = () => css`
-  padding: 40px 20px;
+  width: 100%;
+  height: 100%;
+  padding: 100px 20px 40px;
+`;
+
+export const content = () => css`
+  width: 100%;
+  height: 100%;
+`;
+
+export const description = () => css`
+  text-align: center;
+  line-height: 1.5;
 `;
