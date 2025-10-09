24493af6e6b3393196f285552d88831f091969de
Oliy <eunsoa0310@gmail.com>
2025-10-10 04:43:45 +0900

    fix: 모킹 파일을 추가하여 테스트 환경에서 svg 파일을 문자열로 대체
    - non-JavaScript 파일(이미지, SVG, CSS 등)을 테스트 환경에서 단순한 문자열로 대체하는 모킹 파일 추가
    - Jest 설정에서 이 모킹 파일을 사용하도록 지정

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
     '^@shared/components/(.*)$': '<rootDir>/src/shared/components/$1',
     '^@shared/styles/(.*)$': '<rootDir>/src/shared/styles/$1',
     '^@shared/types/(.*)$': '<rootDir>/src/shared/types/$1',
-    '^@icons/(.*)$': '<rootDir>/assets/icon/$1',
+    '^@icons/(.*)$': '<rootDir>/src/mocks/fileMock.js',
     '^@mocks/(.*)$': '<rootDir>/src/mocks/$1',
     '^@config/(.*)$': '<rootDir>/src/config/$1',
     '^@sb/(.*)$': '<rootDir>/.storybook/$1',
