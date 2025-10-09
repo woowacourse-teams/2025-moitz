0026b5844313fe7c04d2652ce63fbc9ee4616d1a
Oliy <eunsoa0310@gmail.com>
2025-10-10 01:38:55 +0900

    style: loading의 background color 속성을 color token 사용하도록 수정

--- frontend/src/features/loading/components/baseLoading/BaseLoading.styled.ts
+++ frontend/src/features/loading/components/baseLoading/BaseLoading.styled.ts
@@ -1,12 +1,14 @@
 import { css } from '@emotion/react';
 
+import { colorToken } from '@shared/styles/tokens';
+
 export const container = () => css`
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: ${colorToken.blur};
   backdrop-filter: blur(4px);
   z-index: 9999;
 `;
