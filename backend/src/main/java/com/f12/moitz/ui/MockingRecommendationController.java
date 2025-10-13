4fcd7e1f3ea755d254c86ae075b9efe03c3ea68a
DEV_WOONG <ywcsuwon@gmail.com>
2025-10-13 13:50:38 +0900

    refactor: 카테고리 분류에 의한 테스트 변경

--- backend/src/main/java/com/f12/moitz/ui/MockingRecommendationController.java
+++ backend/src/main/java/com/f12/moitz/ui/MockingRecommendationController.java
@@ -10,6 +10,7 @@ import com.f12.moitz.application.dto.RouteResponse;
 import com.f12.moitz.application.dto.StartingPlaceResponse;
 import com.f12.moitz.domain.RecommendCondition;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,16 +47,40 @@ public class MockingRecommendationController implements SwaggerMockingRecommenda
                         new RecommendationResponse(1L, 1, 37.54040751726388, 127.06920291650829, "건대입구역", 18, true,
                                 "다양한 즐길거리가 가득! 🥳 (식당, 카페, PC방, 노래방, 오락시설)",
                                 "어디에서 출발해도 교통이 좋은 중심지이며, 다양한 만남 장소가 존재하는 최적의 장소 중 하나입니다.",
-                                List.of(
-                                        new PlaceRecommendResponse(1, 127.094741101863, 37.5351180385975,
-                                                "육일관", "식당", 1,
-                                                "http://place.map.kakao.com/1050190897"),
-                                        new PlaceRecommendResponse(2, 127.094741101863, 37.5351180385975,
-                                                "바나프레소 건대역점", "카페", 1,
-                                                "http://place.map.kakao.com/666587821"),
-                                        new PlaceRecommendResponse(3, 127.094741101863, 37.5351180385975,
-                                                "포포PC방 건대점", "PC방", 1,
-                                                "http://place.map.kakao.com/356960076")
+                                Map.of(
+                                        "식당", List.of(
+                                                new PlaceRecommendResponse(
+                                                        1,
+                                                        127.094741101863,
+                                                        37.5351180385975,
+                                                        "육일관",
+                                                        "식당",
+                                                        1,
+                                                        "http://place.map.kakao.com/1050190897"
+                                                )
+                                        ),
+                                        "카페", List.of(
+                                                new PlaceRecommendResponse(
+                                                        1,
+                                                        127.094741101863,
+                                                        37.5351180385975,
+                                                        "바나프레소 건대역점",
+                                                        "카페",
+                                                        1,
+                                                        "http://place.map.kakao.com/666587821"
+                                                )
+                                        ),
+                                        "PC방", List.of(
+                                                new PlaceRecommendResponse(
+                                                        1,
+                                                        127.094741101863,
+                                                        37.5351180385975,
+                                                        "포포PC방 건대점",
+                                                        "PC방",
+                                                        1,
+                                                        "http://place.map.kakao.com/356960076"
+                                                )
+                                        )
                                 ),
                                 List.of(
                                         new RouteResponse(1, 0, 4, List.of(
@@ -81,21 +106,27 @@ public class MockingRecommendationController implements SwaggerMockingRecommenda
                         new RecommendationResponse(2L, 2, 37.47656223234824, 126.98155858357366, "사당역", 18, true,
                                 "만남의 광장, 맛집도 다양! 😋 (식당, 카페, PC방)",
                                 "다양한 장소에서 접근하기 편하며, 주어진 카테고리에 대한 요건을 만족하는 가장 추천드리는 선택지입니다.",
-                                List.of(
-                                        new PlaceRecommendResponse(
-                                                1, 127.094741101863, 37.5351180385975,
-                                                "훈장골 사당점", "식당", 1,
-                                                "http://place.map.kakao.com/63778027"
+                                Map.of(
+                                        "식당",List.of(
+                                                new PlaceRecommendResponse(
+                                                        1, 127.094741101863, 37.5351180385975,
+                                                        "훈장골 사당점", "식당", 1,
+                                                        "http://place.map.kakao.com/63778027"
+                                                )
                                         ),
-                                        new PlaceRecommendResponse(
-                                                2, 127.094741101863, 37.5351180385975,
-                                                "스타벅스 사당점", "카페", 1,
-                                                "http://place.map.kakao.com/23447734"
+                                        "카페",List.of(
+                                                new PlaceRecommendResponse(
+                                                        2, 127.094741101863, 37.5351180385975,
+                                                        "스타벅스 사당점", "카페", 1,
+                                                        "http://place.map.kakao.com/23447734"
+                                                )
                                         ),
-                                        new PlaceRecommendResponse(
-                                                3, 127.094741101863, 37.5351180385975,
-                                                "레벨업PC방 사당역점", "PC방", 2,
-                                                "http://place.map.kakao.com/1705311839"
+                                        "PC방",List.of(
+                                                new PlaceRecommendResponse(
+                                                        3, 127.094741101863, 37.5351180385975,
+                                                        "레벨업PC방 사당역점", "PC방", 2,
+                                                        "http://place.map.kakao.com/1705311839"
+                                                )
                                         )
                                 ),
                                 List.of(
@@ -116,20 +147,26 @@ public class MockingRecommendationController implements SwaggerMockingRecommenda
                         new RecommendationResponse(3L, 3, 37.561268363317176, 127.03710337610202, "왕십리역", 18, true,
                                 "교통 요충지, 엔터-식사 해결! ✨ (식당, 카페, PC방, 노래방, 오락시설)",
                                 "어디에서 출발해도 교통이 좋은 중심지이며, 다양한 만남 장소가 존재하는 최적의 장소 중 하나입니다.",
-                                List.of(
-                                        new PlaceRecommendResponse(
-                                                1, 127.094741101863, 37.5351180385975,
-                                                "스시도쿠", "식당", 1,
-                                                "http://place.map.kakao.com/26792732"
+                                Map.of(
+                                        "식당",List.of(
+                                                new PlaceRecommendResponse(
+                                                        1, 127.094741101863, 37.5351180385975,
+                                                        "스시도쿠", "식당", 1,
+                                                        "http://place.map.kakao.com/26792732"
+                                                )
                                         ),
-                                        new PlaceRecommendResponse(
-                                                2, 127.094741101863, 37.5351180385975,
-                                                "탐앤탐스 왕십리역점", "카페", 1,
-                                                "http://place.map.kakao.com/10809505"
+                                        "카페",List.of(
+                                                new PlaceRecommendResponse(
+                                                        2, 127.094741101863, 37.5351180385975,
+                                                        "탐앤탐스 왕십리역점", "카페", 1,
+                                                        "http://place.map.kakao.com/10809505"
+                                                )
                                         ),
-                                        new PlaceRecommendResponse(3, 127.094741101863, 37.5351180385975,
-                                                "이스포츠PC방 왕십리점", "PC방", 2,
-                                                "http://place.map.kakao.com/12326220"
+                                        "PC방",List.of(
+                                                new PlaceRecommendResponse(3, 127.094741101863, 37.5351180385975,
+                                                        "이스포츠PC방 왕십리점", "PC방", 2,
+                                                        "http://place.map.kakao.com/12326220"
+                                                )
                                         )
                                 ),
                                 List.of(
@@ -158,18 +195,24 @@ public class MockingRecommendationController implements SwaggerMockingRecommenda
                         new RecommendationResponse(4L, 4, 37.570227990912244, 126.98315081716676, "종각역", 24, false,
                                 "젊음의 거리, 핫플집합소! 😉 (식당, 카페, PC방, 노래방)",
                                 "다양한 장소에서 접근하기 편하며, 주어진 카테고리에 대한 요건을 만족하는 가장 추천드리는 선택지입니다.",
-                                List.of(
-                                        new PlaceRecommendResponse(1, 127.094741101863, 37.5351180385975,
-                                                "스타벅스 종로R점", "카페", 1,
-                                                "http://place.map.kakao.com/1784996243"
+                                Map.of(
+                                        "식당",List.of(
+                                                new PlaceRecommendResponse(2, 127.094741101863, 37.5351180385975,
+                                                        "한우공방", "식당", 1,
+                                                        "http://place.map.kakao.com/886708185"
+                                                )
                                         ),
-                                        new PlaceRecommendResponse(2, 127.094741101863, 37.5351180385975,
-                                                "한우공방", "식당", 1,
-                                                "http://place.map.kakao.com/886708185"
+                                        "카페",List.of(
+                                                new PlaceRecommendResponse(1, 127.094741101863, 37.5351180385975,
+                                                        "스타벅스 종로R점", "카페", 1,
+                                                        "http://place.map.kakao.com/1784996243"
+                                                )
                                         ),
-                                        new PlaceRecommendResponse(3, 127.094741101863, 37.5351180385975,
-                                                "옵티멈존 PC카페 종각역점", "PC방", 1,
-                                                "http://place.map.kakao.com/1342335656"
+                                        "PC방",List.of(
+                                                new PlaceRecommendResponse(3, 127.094741101863, 37.5351180385975,
+                                                        "옵티멈존 PC카페 종각역점", "PC방", 1,
+                                                        "http://place.map.kakao.com/1342335656"
+                                                )
                                         )
                                 ),
                                 List.of(
@@ -205,21 +248,26 @@ public class MockingRecommendationController implements SwaggerMockingRecommenda
                         ),
                         new RecommendationResponse(5L, 5, 37.5568707448873, 126.923778562273, "홍대입구역", 27, false,
                                 "젊음의 거리, 놀거리 천국! 😎 (식당, 카페, PC방, 노래방, 오락시설)", "젊음과 문화의 거리인 홍대입구, 적절히 요소들을 잘 고려했습니다.",
-                                List.of(
-                                        new PlaceRecommendResponse(
-                                                1, 127.094741101863, 37.5351180385975,
-                                                "하이디라오 홍대지점", "음식점", 1,
-                                                "http://place.map.kakao.com/1622865435"),
-
-                                        new PlaceRecommendResponse(
-                                                2, 127.094741101863, 37.5351180385975,
-                                                "1984", "카페", 1,
-                                                "http://place.map.kakao.com/23634722"
+                                Map.of(
+                                        "식당",List.of(
+                                                new PlaceRecommendResponse(
+                                                        1, 127.094741101863, 37.5351180385975,
+                                                        "하이디라오 홍대지점", "음식점", 1,
+                                                        "http://place.map.kakao.com/1622865435")
                                         ),
-                                        new PlaceRecommendResponse(
-                                                3, 127.094741101863, 37.5351180385975,
-                                                "에스엔에스 피씨SNS PC", "PC방", 1,
-                                                "http://place.map.kakao.com/798252372"
+                                        "카페",List.of(
+                                                new PlaceRecommendResponse(
+                                                        2, 127.094741101863, 37.5351180385975,
+                                                        "1984", "카페", 1,
+                                                        "http://place.map.kakao.com/23634722"
+                                                )
+                                        ),
+                                        "PC방",List.of(
+                                                new PlaceRecommendResponse(
+                                                        3, 127.094741101863, 37.5351180385975,
+                                                        "에스엔에스 피씨SNS PC", "PC방", 1,
+                                                        "http://place.map.kakao.com/798252372"
+                                                )
                                         )
                                 ),
                                 List.of(
