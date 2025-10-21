7d41632f92d33564dc3d4e754eefe1b700907d91
DEV_WOONG <ywcsuwon@gmail.com>
2025-10-21 09:52:16 +0900

    refactor: 메서드 인자에 final 추가

--- backend/src/main/java/com/f12/moitz/infrastructure/client/kakao/dto/SearchImageRequest.java
+++ backend/src/main/java/com/f12/moitz/infrastructure/client/kakao/dto/SearchImageRequest.java
@@ -9,7 +9,7 @@ public class SearchImageRequest{
     private final int page;
     private final int size;
 
-    public SearchImageRequest(String stationName, String place, int page, int size) {
+    public SearchImageRequest(final String stationName, final String place, final int page, final int size) {
         this.query = stationName + " " + place;
         this.page = page;
         this.size = size;
