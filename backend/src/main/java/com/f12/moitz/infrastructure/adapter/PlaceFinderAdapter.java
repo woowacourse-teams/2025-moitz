57ee75e6fbe0e5cb6a817c8121868861d2c054b7
egaeng09 <egaeng09@gmail.com>
2025-11-25 00:30:52 +0900

    refactor: Adapter 네이밍 변경

--- backend/src/main/java/com/f12/moitz/infrastructure/adapter/PlaceFinderAdapter.java
+++ backend/src/main/java/com/f12/moitz/infrastructure/adapter/PlaceFinderAdapter.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class KakaoPlaceFinderAdapter implements PlaceFinder {
+public class PlaceFinderAdapter implements PlaceFinder {
 
     private final KakaoMapClient kakaoMapClient;
 
