8e7b1c091baf97787e3b6daa7bb51e6eca318c91
saera <yook.saera@gmail.com>
2025-12-04 16:56:57 +0900

    refactor: 메서드 재정렬

--- backend/src/main/java/com/f12/moitz/domain/subway/SubwayEdges.java
+++ backend/src/main/java/com/f12/moitz/domain/subway/SubwayEdges.java
@@ -29,25 +29,9 @@ public class SubwayEdges {
         return new SubwayEdges(subwayEdgeSet);
     }
 
-    public Set<Edge> getEdges(final SubwayStation currentStation) {
-        return subwayEdges.stream()
-                .filter(edgeSet -> edgeSet.isSameStation(currentStation))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("현재 역에 해당하는 SubwayEdge가 존재하지 않습니다. 역 이름: " + currentStation.getName()))
-                .getEdges();
-    }
-
     public boolean isContainsStation(final SubwayStation station) {
         return subwayEdges.stream()
                 .anyMatch(edgeSet -> edgeSet.isSameStation(station));
-    }
-
-    public Set<SubwayStation> getAllStations() {
-        final Set<SubwayStation> stations = new HashSet<>();
-        for (SubwayEdge edgeSet : subwayEdges) {
-            stations.add(edgeSet.getSubwayStation());
-        }
-        return stations;
     }
 
     public SubwayStation getStationByName(final String stationName) {
@@ -56,6 +40,14 @@ public class SubwayEdges {
                 .filter(station -> station.isNameMatch(stationName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("이름이 일치하는 SubwayStation이 존재하지 않습니다: " + stationName));
+    }
+
+    public Set<SubwayStation> getAllStations() {
+        final Set<SubwayStation> stations = new HashSet<>();
+        for (SubwayEdge edgeSet : subwayEdges) {
+            stations.add(edgeSet.getSubwayStation());
+        }
+        return stations;
     }
 
     public void addEdge(final SubwayStation subwayStation, final Edge edge) {
@@ -77,6 +69,14 @@ public class SubwayEdges {
         return subwayEdges.stream()
                 .flatMap(subwayEdge -> subwayEdge.findEdgeBy(from, to, line).stream())
                 .findFirst();
+    }
+
+    public Set<Edge> getEdges(final SubwayStation station) {
+        return subwayEdges.stream()
+                .filter(edgeSet -> edgeSet.isSameStation(station))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("현재 역에 해당하는 SubwayEdge가 존재하지 않습니다. 역 이름: " + station.getName()))
+                .getEdges();
     }
 
 }
