d58fdc9c2f774a03a3a1bf4c706e16cf6296255f
egaeng09 <egaeng09@gmail.com>
2026-05-01 15:55:02 +0900

    test: 테스트 기본 프로필 분리

--- backend/src/test/java/com/f12/moitz/MoitzApplicationTests.java
+++ backend/src/test/java/com/f12/moitz/MoitzApplicationTests.java
@@ -1,10 +1,19 @@
 package com.f12.moitz;
 
+import com.f12.moitz.application.SetupService;
+import com.f12.moitz.domain.repository.SubwayEdgeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class MoitzApplicationTests {
+
+    @MockitoBean
+    private SubwayEdgeRepository subwayEdgeRepository;
+
+    @MockitoBean
+    private SetupService setupService;
 
     @Test
     void contextLoads() {
